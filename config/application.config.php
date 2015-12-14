9a9b6762b3d0ee21a51da0f38f80ca32d07e437e
corner82 <311corner82@gmail.com>
2015-12-14 16:07:01 +0200

    Yeni Yönetim paneli modülü eklendi
    Yönetim paneli modülü konfigürasyon dosyasına eklendi

--- config/application.config.php
+++ config/application.config.php
@@ -11,6 +11,7 @@ return array(
         'Application',
         'Admin',
         'Login',
+        'SFDM',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
