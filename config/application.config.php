f75a0713bbb7031471813f6a843e879499b0d102
corner82 <311corner82@gmail.com>
2015-12-15 10:24:01 +0200

    Sanal fabrika önyüz modülü konfigürasyona eklendi
    Sanal fabrika önyüz modülü konfigürasyona eklendi

--- config/application.config.php
+++ config/application.config.php
@@ -12,6 +12,7 @@ return array(
         'Admin',
         'Login',
         'SFDM',
+        'SanalFabrika'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
