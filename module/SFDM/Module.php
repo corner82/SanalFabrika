f1a1badabefe470f6b81b330f8f495ffed3b1592
corner82 <311corner82@gmail.com>
2015-12-21 10:32:41 +0200

    code açıklaması eklendi
    code açıklaması eklendi

--- module/SFDM/Module.php
+++ module/SFDM/Module.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * OSTİM TEKNOLOJİ Framework 
+ *
+ * @link      https://github.com/corner82/slim_test for the canonical source repository
+ * @copyright Copyright (c) 2015 OSTİM TEKNOLOJİ (http://www.ostim.com.tr)
+ * @license   
+ */
 namespace SFDM;
 
  use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
