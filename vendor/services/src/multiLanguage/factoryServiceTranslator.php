a748dd9f2e34950ce13832b31407648bcc65b5d5
corner82 <311corner82@gmail.com>
2015-12-21 12:02:09 +0200

    factory service yeni sabitlere göre düzenlendi
    factory service yeni sabitlere göre düzenlendi

--- vendor/services/src/multiLanguage/factoryServiceTranslator.php
+++ vendor/services/src/multiLanguage/factoryServiceTranslator.php
@@ -1,11 +1,22 @@
 <?php
+/**
+ * OSTİM TEKNOLOJİ Framework 
+ *
+ * @link      https://github.com/corner82/slim_test for the canonical source repository
+ * @copyright Copyright (c) 2015 OSTİM TEKNOLOJİ (http://www.ostim.com.tr)
+ * @license   
+ */
 
 namespace Custom\Services\MultiLanguage;
 
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
+use Custom\Services\MultiLanguage\systemLanguages;
 
-class factoryServiceTranslator  implements FactoryInterface{
+/**
+ * using tarnslator as service in zend service manager
+ */
+class FactoryServiceTranslator  implements FactoryInterface{
 
     public function createService(ServiceLocatorInterface $serviceLocator) {
 
@@ -14,16 +25,42 @@ class factoryServiceTranslator  implements FactoryInterface{
         
         $lang = $event->getRouteMatch()
                       ->getParam('lang');
+        $lang = trim(strtolower($lang));
+        
         $translator = $event->getApplication()
                             ->getServiceManager()
                             ->get('translator');
-        if($lang == 'eng'){
-            $translator->setLocale('en_EN');
-        } else if($lang == 'ru') {
-            $translator->setLocale('ru_RU');
-        } else if($lang == null) {
+        
+        $systemLanguageCodes = systemLanguages::getSystemLanguageCodes();
+
+        if(in_array($lang, $systemLanguageCodes)) {
+            switch ($lang) {
+                case systemLanguages::ENG : 
+                    $translator->setLocale(systemLanguages::ENG_LOCALE);
+                    break;
+                case systemLanguages::AR : 
+                    $translator->setLocale(systemLanguages::AR_LOCALE);
+                    break;
+                case systemLanguages::DE : 
+                    $translator->setLocale(systemLanguages::DE_LOCALE);
+                    break;
+                case systemLanguages::RU : 
+                    $translator->setLocale(systemLanguages::RU_LOCALE);
+                    break;
+                case systemLanguages::TR : 
+                    $translator->setLocale(systemLanguages::TR_LOCALE);
+                    break;
+                case systemLanguages::FA : 
+                    $translator->setLocale(systemLanguages::FA_LOCALE);
+                    break;
+                default:
+                    $translator->setLocale(systemLanguages::TR_LOCALE);
+            }
+            
+        } else {
             $translator->setLocale('tr_TR');
         }
+
         return false;
 
     }
