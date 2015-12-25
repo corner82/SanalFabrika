a92242bbfcf4bc247fd47bc0085e7b6ac80eccff
corner82 <311corner82@gmail.com>
2015-12-25 10:36:53 +0200

    servis çağırılırken gereksiz parametre ve özellikler silindi
    servis çağırılırken gereksiz parametre ve özellikler silindi

--- public/jsinline/sanalfabrika.js
+++ public/jsinline/sanalfabrika.js
@@ -8,13 +8,11 @@ $(document).ready(function () {
     $.ajax({
         url: 'https://proxy.sanalfabrika.com/SlimProxyBoot.php',              
         data: {
-            parent: 0,
-            pk : '3441df0babc2a2dda551d7cd39fb235bc4e09cd1e4556bf261bb49188f548348',
-            url : 'fillComboBox_syslanguage',
-            language_id: 647                           
+            //pk : '3441df0babc2a2dda551d7cd39fb235bc4e09cd1e4556bf261bb49188f548348',
+            url : 'fillComboBox_syslanguage',                          
             },
         method: "GET",
-        async: false,
+        //async: false,
         dataType: "json",
         success: function (data) {
             var data = data;
