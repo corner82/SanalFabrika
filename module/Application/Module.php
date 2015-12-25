cbca979d0ba6c4e869bfdbb2bff0ce312ba2fd2d
corner82 <311corner82@gmail.com>
2015-12-25 14:17:27 +0200

    onbootstarp metodu içerisine yorum code bloğu eklendi
    onbootstarp metodu içerisine yorum code bloğu eklendi, örnek koddur
    silmeyiniz

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -18,6 +18,41 @@ class Module
     {
         /*$translator = $e->getApplication()->getServiceManager()->get('translator');
         $translator->setLocale('ru_RU');*/
+        
+        /**
+         * that code block is an example of advanced usage of dispath error event
+         * types, do not erase code block!!!!!!
+         * @author Mustafa Zeynel Dağlı
+         * @since 25/12/2015
+         *
+         */
+        /*$eventManager->attach(MvcEvent::EVENT_DISPATCH_ERROR, function($e) {
+        $error = $e->getError();
+        switch ($error) {
+            case \Zend\Mvc\Application::ERROR_CONTROLLER_NOT_FOUND:
+            case \Zend\Mvc\Application::ERROR_CONTROLLER_INVALID:
+            case \Zend\Mvc\Application::ERROR_ROUTER_NO_MATCH:
+                print_r('ERROR_ROUTER_NO_MATCH');
+                $response = $e->getResponse();
+               /* $lang = $e->getRouter()
+                          //->getParam('lang');
+                        //->getParams();*/
+                //$e->getRouter()->getRoute();
+                
+                /*$request = $e->getRequest();
+                $requestMeta = $request->getMetadata();
+                $requesturi = $request->getUri();
+                print_r($requesturi);
+                $response = $e->getResponse();
+                $response->getHeaders()->addHeaderLine('Location', '/?lang=12');
+                $response->getHeaders()->addHeaderLine('lang', '12');
+                //$response->setStatusCode(302);
+                print_r($response->getHeaders('Location'));
+                return $response;
+            break;
+        }
+    }, 100);*/
+        
     }
 
     public function getConfig()
