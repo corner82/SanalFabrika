1e5e113b98dd6b1cbbeb9b999083756081deec48
Bahram <bahram.lotfisadigh@gmail.com>
2016-01-22 13:49:51 +0200

    registration

--- public/onyuz/standard/assets/js/plugins/masking.js
+++ public/onyuz/standard/assets/js/plugins/masking.js
@@ -1,20 +1,30 @@
 var Masking = function () {
 
     return {
-        
         //Masking
         initMasking: function () {
-	        $("#date1").mask('99/99/9999', {placeholder:'X'});
-	        $("#phone").mask('(999) 999-9999', {placeholder:'X'});
-	        $("#card").mask('9999-9999-9999-9999', {placeholder:'X'});
-	        $("#serial").mask('***-***-***-***-***-***', {placeholder:'_'});
-	        $("#tax").mask('99-9999999', {placeholder:'X'});
-                $("#userPostalCode").mask('99999', {placeholder:'X'});
-	        $("#userPhone").mask('(999) 999-9999', {placeholder:'X'});
-	        $("#userGSM").mask('(999) 999-9999', {placeholder:'X'});
-	        $("#userFax").mask('(999) 999-9999', {placeholder:'X'});
+            $("#date1").mask('99/99/9999', {placeholder: 'X'});
+            $("#phone").mask('(999) 999-9999', {placeholder: 'X'});
+            $("#card").mask('9999-9999-9999-9999', {placeholder: 'X'});
+            $("#serial").mask('***-***-***-***-***-***', {placeholder: '_'});
+            $("#tax").mask('99-9999999', {placeholder: 'X'});
+            $("#userPostalCode").mask('99999', {placeholder: 'X'});
+            $("#userPhone").mask('(999) 999-9999', {placeholder: 'X'});
+            $("#userGSM").mask('(999) 999-9999', {placeholder: 'X'});
+            $("#userFax").mask('(999) 999-9999', {placeholder: 'X'});
+            
+            if(selectedCountry == 'Turkey'){
+                
+            }
+
+
+            $("#companyTaxNumber").mask('9999999999', {placeholder: 'X'});
+            if (selectedCountry == 'Turkey') {
+                
+                
+            }
         }
 
     };
-    
+
 }();
