b238eaa274f66a74427b03a9be116b2aefcdacc5
Bahram <bahram.lotfisadigh@gmail.com>
2016-02-25 18:00:18 +0200

    cmt
    Alpaca form creation

--- public/jsinline/sanalFabrika_cmt.js
+++ public/jsinline/sanalFabrika_cmt.js
@@ -11,107 +11,21 @@ $(document).ready(function () {
         defaultLang: 'en'
     });
     lang.change($('#langCode').val());
-
     // Left menuyu oluşturmak için çağırılan fonksiyon...
     $.fn.leftMenuFunction();
 
-    /*
-     * Machine tools list tree
-     * @author:Bahram Lotfi Sadigh
-     * @Since: 2016.2.18
-     */
-
-    $("#mtPropsDynamicForm").alpaca({
-        "schema": {
-            "type": "object",
-            "properties": {
-                "generalMachineInformation": {
-                    "type": "object",
-                    "properties": {
-                        "manufacturer": {
-                            "type": "string"
-                        },
-                        "name": {
-                            "type": "string"
-                        },
-                        "model": {
-                            "type": "string"
-                        },
-                        "type": {
-                            "type": "string"
-                        }
-                    }
-                },
-                "detailedMachineProperties": {
-                    "type": "object",
-                    "properties": {
-                        "machineProperties": {
-                            "type": "string"
-                        }
-                    }
-                }
-            }
-        },
-        "options": {
-            "fields": {
-                "generalMachineInformation": {
-                    "label": "General Machine Information ",
-                    "fields": {
-                        "manufacturer": {
-                            "label": "Machine Manufacturer",
-                            "type": "text"
-                        },
-                        "name": {
-                            "label": "Machine Name",
-                            "type": "text"
-                        },
-                        "model": {
-                            "label": "Machine Model",
-                            "type": "text"
-                        },
-                        "type": {
-                            "label": "Machine Type",
-                            "type": "text"
-                        }
-                    }
-                },
-                "detailedMachineProperties": {
-                    "label": "Detailed Machine Properties",
-                    "fields": {
-                        "machineProperties": {
-                            "label": "Machine Properties",
-                            "type": "text"
-                        }
-                    }
-                }
-            }
-        },
-        "data": {
-            "manufacturer": "Machine Manufacturer",
-            "name": "Machine Name",
-            "model": "Machine Model",
-            "type": "Machine Category",
-            "machineProperties": "Machine Properties"
-        },
-        "postRender": function (control) {
-//            var manufacturerField = control.childrenByPropertyId["name"];
-//            var nameField = control.childrenByPropertyId["job"];
-
-        }
-    });
-
+    window.selectedRow;
+    window.rowIndex;
 
 
 
 
     var tree = $('.tree2').machineTree();
-
     tree.machineTree('option', 'url', 'pkFillMachineToolGroups_sysMachineToolGroups');
     tree.machineTree('option', 'pk', $("#pk").val());
     tree.machineTree('option', 'language_code', $("#langCode").val());
+//    tree.machineTree('option', 'profile_id', 97);
     tree.machineTree('setMainRoot');
-
-
     /**
      * machine tool tree
      * @author Mustafa Zeynel Dağlı
@@ -120,7 +34,7 @@ $(document).ready(function () {
 
     $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
     $('.tree li.parent_li > span').on('click', function (e) {
-        // alert('test');
+// alert('test');
         var children = $(this).parent('li.parent_li').find(' > ul > li');
         if (children.is(":visible")) {
             children.hide('fast');
@@ -133,6 +47,239 @@ $(document).ready(function () {
         }
         e.stopPropagation();
     });
+    /**
+     * grid_confirm_registration easyui datagrid
+     * user confirmation datagrid listing for confirmation
+     * @author Mustafa Zeynel Dağlı
+     * @since 10/02/2016
+     */
 
+
+    $('#grid_company_machines').datagrid({
+        onDblClickRow: function (index, row) {
+            selectedRow = $("#grid_company_machines").datagrid("getSelected");
+            rowIndex = $("#grid_company_machines").datagrid("getRowIndex", selectedRow);
+            gridMachineProperties(this);
+            $('.nav-tabs a[href="#tab_mt_properties"]').tab('show');
+//            alert('test');
+        },
+        url: 'https://proxy.sanalfabrika.com/SlimProxyBoot.php',
+        queryParams: {url: 'pkFillUsersFirmMachines_infoFirmMachineTool',
+            pk: $('#pk').val(),
+            language_code: $("#langCode").val()
+        },
+        //url: 'http://proxy.localhost.com/SlimProxyBoot.php?url=getCompaniesInfo_company',
+        width: '100%',
+        singleSelect: true,
+        pagination: true,
+        collapsible: true,
+        method: 'get',
+        idField: 'id',
+//        toolbar:'#tb5',
+//        fit:true,
+//        fitColumns: true,
+        remoteFilter: true,
+        remoteSort: true,
+        multiSort: false,
+        sortable: true,
+        frozenColumns:
+                [[
+                        {field: 'machine_id', title: window.lang.translate('Machine ID'), sortable: true}
+
+                    ]],
+        columns:
+                [[
+                        {field: 'manufacturer_name', title: window.lang.translate('Machine Manufacturer'), sortable: true},
+                        {field: 'machine_tool_names', title: window.lang.translate('Machine Name'), sortable: true},
+                        {field: 'machine_tool_grup_names', title: window.lang.translate('Machine Category'), sortable: true},
+                        {field: 'model', title: window.lang.translate('Machine Model'), sortable: true},
+                        {field: 'model_year', title: window.lang.translate('Machine Production Year'), sortable: true}
+                    ]]
+    });
+    /**
+     * trying to get row index from easyui grid
+     * @param {type} target
+     * @returns integer
+     * @author Mustafa Zeynel Dağlı
+     * @since 09/02/2016
+     */
+
+    window.getRowIndex = function (target) {
+        var tr = $(target).closest('tr.datagrid-row');
+        return parseInt(tr.attr('datagrid-row-index'));
+    };
+    /**
+     * 'grid_confirm_registration' easyui grid detail click function
+     * @param {type} target
+     * @returns {undefined}
+     * @author Mustafa Zeynel Dağlı
+     * @since 09/02/2016
+     */
+
+    window.gridMachineProperties = function (target) {
+//        console.log(selectedRow.machine_id);
+        $.ajax({
+            url: 'https://proxy.sanalfabrika.com/SlimProxyBoot.php',
+            data: {url: 'pkFillUsersFirmMachineProperties_infoFirmMachineTool',
+                pk: $("#pk").val(),
+                machine_id: selectedRow.machine_id},
+            type: 'GET',
+            dataType: 'json',
+            success: function (data, textStatus, jqXHR) {
+//                console.log(data);
+
+//                console.log(data.rows.length);
+
+                if (data.rows.length !== 0) {
+
+                    $("#mtGenPropsDynamicForm").alpaca({
+                        "schema": {
+                            "type": "object",
+                            "properties": {
+                                "generalMachineInformation": {
+                                    "type": "object",
+                                    "properties": {
+                                        "manufacturer": {
+                                            "type": "string"
+                                        },
+                                        "name": {
+                                            "type": "string"
+                                        },
+                                        "model": {
+                                            "type": "string"
+                                        },
+                                        "type": {
+                                            "type": "string"
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        "options": {
+                            "fields": {
+                                "generalMachineInformation": {
+                                    "label": window.lang.translate("General Machine Information"),
+                                    "fields": {
+                                        "manufacturer": {
+                                            "label": window.lang.translate("Machine Manufacturer"),
+                                            "type": "text",
+                                            "disabled": true
+                                        },
+                                        "name": {
+                                            "label": window.lang.translate("Machine Name"),
+                                            "type": "text",
+                                            "disabled": true
+                                        },
+                                        "model": {
+                                            "label": window.lang.translate("Machine Model"),
+                                            "type": "text",
+                                            "disabled": true
+                                        },
+                                        "type": {
+                                            "label": window.lang.translate("Machine Type"),
+                                            "type": "text",
+                                            "disabled": true
+                                        }
+                                    }
+                                }
+//                            }
+                        },
+                        "data": {
+                            "manufacturer": selectedRow.manufacturer_name,
+                            "name": selectedRow.machine_tool_names,
+                            "model": selectedRow.model_year,
+                            "type": selectedRow.machine_tool_grup_names
+                        }
+                    }
+                    });
+
+                    for (var i = 0; i < data.rows.length; i++) {
+
+                        var property_name = data.rows[i].property_names;
+                        var property_value = data.rows[i].property_value;
+                        var property_unit = data.rows[i].unitcodes;
+                        var property_name_english = data.rows[i].property_name_eng;
+
+                        /*
+                         * Machine tools list tree
+                         * @author:Bahram Lotfi Sadigh
+                         * @Since: 2016.2.18
+                         */
+                        if (property_name !== null) {
+
+                            $("#mtSpecPropsDynamicForm").alpaca({
+                                "schema": {
+                                    "type": "object",
+                                    "properties": {
+                                        property_name: {
+                                            "type": "string"
+                                        }
+                                    }
+                                },
+                                "options": {
+                                    "fields": {
+                                        property_name: {
+                                            "label": property_name,
+                                            "type": "text",
+                                            "helper": property_name_english,
+                                            "disabled": true                                           
+                                        }
+                                    }
+                                },
+                                "data": {
+                                    property_name: property_value + '  ' + property_unit
+                                }
+                            });
+                        }
+                    }
+                    
+                    if ($('#tab_confirm_image_loader').loadImager() !== 'undefined') {
+                        $('#tab_confirm_image_loader').loadImager('removeLoadImage');
+                        $('#tab_confirm_container a[href="#tab_confirm"]').tab('show');
+
+                    }
+                } else {
+                    console.log('error');
+                    BootstrapDialog.alert({
+                        title: 'DİKKAT!! Kullanıcı detayları belirlenememiştir',
+                        message: 'Kullanıcı dateyları belirlenememiştir, lütfen başka kullanıcı seçiniz!!',
+                        type: BootstrapDialog.TYPE_WARNING, // <-- Default value is BootstrapDialog.TYPE_PRIMARY
+                        closable: true, // <-- Default value is false
+                        draggable: true, // <-- Default value is false
+                        buttonLabel: 'Tamam' // <-- Default value is 'OK',
+                    });
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+
+                console.log('error');
+                console.error(textStatus);
+            }
+        });
+    };
+
+
+    /**
+     * Machine properties tab blocker
+     * @author Bahram Lotfi
+     * @since 25/02/2016
+     */
+    $('#machineToolsPropertiesTab').click(function (e) {
+        if (!$('#tab_mt_properties').hasClass('active')) {
+            BootstrapDialog.alert({
+                title: window.lang.translate('Warning'),
+                message: window.lang.translate('Please first select a machine tool from machine tools list'),
+                description: window.lang.translate('Select a machine tool to view machine information details'),
+                type: BootstrapDialog.TYPE_WARNING, // <-- Default value is BootstrapDialog.TYPE_PRIMARY
+                closable: true, // <-- Default value is false
+                draggable: true, // <-- Default value is false
+                buttonLabel: 'OK' // <-- Default value is 'OK',
+            });
+            $('#tab_mt_properties_image_loader').loadImager();
+            $('#tab_mt_properties_image_loader').loadImager('appendImage');
+            //$('#tab_confirm_container a:first').tab('show');
+        }
+        e.preventDefault();
+    });
 
 });
