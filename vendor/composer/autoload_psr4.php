e9f94db6c1b59489e7b9af260da49618735e88cc
corner82 <311corner82@gmail.com>
2016-03-14 12:09:41 +0200

    messaging lib class ce değişen auto load class eklendi
    messaging lib class ce değişen auto load class eklendi

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -67,4 +67,5 @@ return array(
     'Custom\\Services\\MultiLanguage\\' => array($vendorDir . '/services/src/multiLanguage'),
     'Custom\\Services\\Filter\\' => array($vendorDir . '/services/src/filter'),
     'Custom\\Services\\Acl\\' => array($vendorDir . '/services/src/acl'),
+    'Utill\\MQ\\' => array($vendorDir . '/utill/mq'),
 );
