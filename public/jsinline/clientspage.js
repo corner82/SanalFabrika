75a3f677490c9becc076751bede0fbe3b60fa441
Bahram <bahram.lotfisadigh@gmail.com>
2016-12-28 15:23:48 +0200

    ayarlar sayfa basliginda

--- public/jsinline/clientspage.js
+++ public/jsinline/clientspage.js
@@ -11,16 +11,29 @@ $(document).ready(function () {
 /*
      * Checking pk value for login and logout actions
      */
-    if (!$('#pk').val()) {
-        $('#login_place').css('visibility', 'visible');
-        $('#login_place').css('display', '');
-        $('#logout_place').css('visibility', 'hidden');
-        $('#logout_place').css('display', 'none');
-    } else {
+    if ($('#pk').val()) {
+        
         $('#login_place').css('visibility', 'hidden');
         $('#login_place').css('display', 'none');
         $('#logout_place').css('visibility', 'visible');
         $('#logout_place').css('display', '');
+        
+        $('#setting_link').css('visibility', 'visible');
+        $('#setting_link_divider').css('visibility', 'visible');
+        $('#setting_link').css('display', '');
+        $('#setting_link_divider').css('display', '');
+        
+    } else {
+                
+        $('#login_place').css('visibility', 'visible');
+        $('#login_place').css('display', '');
+        $('#logout_place').css('visibility', 'hidden');
+        $('#logout_place').css('display', 'none');
+
+        $('#setting_link').css('visibility', 'hidden');
+        $('#setting_link_divider').css('visibility', 'hidden');
+        $('#setting_link').css('display', 'none');
+        $('#setting_link_divider').css('display', 'none');
     }
 
 
