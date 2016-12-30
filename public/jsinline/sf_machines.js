5128201168dcbb6f16a8255ce0e838a6c754700d
Bahram <bahram.lotfisadigh@gmail.com>
2016-12-30 14:02:09 +0200

    machine park page

--- public/jsinline/sf_machines.js
+++ public/jsinline/sf_machines.js
@@ -5,65 +5,64 @@ $(document).ready(function () {
     lang.init({
         defaultLang: 'en'
     });
-
     lang.change($('#langCode').val());
-
-
-
     $('#reg_address_table').datagrid({
         onDblClickRow: function (index, row) {
             $("#machine_details_table").empty();
-            
-            $.ajax({
+            $('#machine_name').empty();
+            $('#machine_image_ph').attr('href', '');
+            $('#machine_image_ph').attr('title', '');
+            $('#machine_image_ph_src').attr('src', '');
+
+
+            $('#machine_name').append(row.machine_tool_name);
+            if (row.attributes.picture !== "") {
+                $('#machine_image_ph').css('visibility', 'visible');
+                $('#machine_image_ph').css('display', '');
+                $('#machine_image_ph').attr('href', "/onyuz/standard/assets/img/sfClients/" + row.attributes.picture);
+                $('#machine_image_ph').attr('title', row.machine_tool_name);
+                $('#machine_image_ph_src').attr('src', "/onyuz/standard/assets/img/sfClients/" + row.attributes.picture);
+            }else{
+                $('#machine_image_ph').css('visibility', 'hidden');
+                $('#machine_image_ph').css('display', 'none');                
+            }
+            $('#mach_props_table').datagrid({
                 url: 'https://proxy.sanalfabrika.com/SlimProxyBoot.php',
-                //                url: 'http://proxy.sanalfabrika.com:9990/SlimProxyBoot.php',            
-                data: {url: 'pkGetMachineProperities_sysMachineTools',
-                    language_code: $('#langCode').val(),
+                //url: 'http://proxy.localhost.com/SlimProxyBoot.php?url=getCompaniesInfo_company',
+                queryParams: {
+                    url: 'pkGetMachineProperities_sysMachineTools',
                     pk: $('#pk').val(),
+                    subject: 'datagrid',
                     machine_id: row.id
+                            /*machine_groups_id : null,
+                             filterRules:null*/
                 },
-                method: "GET",
-                dataType: "json",
-                success: function (data) {
-                    if (data.length > 0) {
-                        console.log(data);
-                        for (var i = 0; i < data.length; i++) {
-
-                            var appending_prop =
-                                    "<tr>"
-                                    + "<td>"
-                                    + data[i].property_name
-                                    + "</td>"
-                                    + "<td>"
-                                    + data[i].property_value
-                                    + "</td>"
-                                    + "<td>"
-                                    + data[i].unitcode
-                                    + "</td>"
-                                    + "</tr>";
-
-                            $('#machine_details_table').append(appending_prop);
-                        }
-
-
-                        $('#table_row').css('visibility', 'visible');
-                        $('#table_row').css('display', 'block');
-
-                        if ($('#table_row').css('visibility') === 'visible') {
-                            $('html, body').animate({
-                                scrollTop: $("#machine_details_table").offset().top
-                            }, 1000);
-                        }
-                    } else {
-                        console.log('no data');
-                    }
-
-                },
-                error: function (jqXHR, textStatus, errorThrown) {                     //console.log('error');
-                    console.error(textStatus);
-                }
+                width: 'auto',
+                singleSelect: true,
+                pagination: false,
+                collapsible: true,
+                method: 'get',
+                idField: 'id',
+                //toolbar:'#tb5',
+//                fit:true,         
+//                fitColumns : true,
+                remoteFilter: true,
+                remoteSort: true,
+                multiSort: false,
+                columns: [[
+                        {field: 'property_name', title: 'Property Name', width: 300},
+                        {field: 'property_value', title: 'Property Value', width: 200, align: 'center'},
+                        {field: 'unitcode', title: 'Unit', width: 160, align: 'center'}
+                    ]]
             });
 
+            $('#props_table_div').css('visibility', 'visible');
+            $('#props_table_div').css('display', '');
+            if ($('#props_table_div').css('visibility') === 'visible') {
+                $('html, body').animate({
+                    scrollTop: $("#props_table_div").offset().top
+                }, 1000);
+            }
         },
         url: 'https://proxy.sanalfabrika.com/SlimProxyBoot.php',
         //url: 'http://proxy.localhost.com/SlimProxyBoot.php?url=getCompaniesInfo_company',
@@ -74,12 +73,12 @@ $(document).ready(function () {
                     /*machine_groups_id : null,
                      filterRules:null*/
         },
-        width: '100%',
+        width: 'auto',
         singleSelect: true,
         pagination: true,
         collapsible: true,
         method: 'get',
-        idField: 'firm_id',
+        idField: 'id',
         //toolbar:'#tb5',
         //fit:true,         //fitColumns : true,
         remoteFilter: true,
@@ -88,7 +87,7 @@ $(document).ready(function () {
         columns: [[
                 {field: 'id', title: 'Id', width: 50},
                 {field: 'machine_tool_name', title: 'Machine Name', width: 200},
-                {field: 'manufacturer_name', title: 'Manufacturer', width: 150, align: 'center'},
+                {field: 'manufacturer_name', title: 'Manufacturer', width: 190, align: 'center'},
                 {field: 'group_name', title: 'Group', width: 150},
                 {field: 'group_name_eng', title: 'Group', width: 150},
                 {field: 'model_year', title: 'Year', width: 100, align: 'center',
@@ -96,15 +95,13 @@ $(document).ready(function () {
                         return row.attributes.model_year;
                     }
                 }
-                //                {field: 'picture', title: 'Picture', width: 300, align: 'center',
-                //                    formatter: function (value, row, index) {
-//                        var image_address = row.attributes.picture;
-//                        var image_place_holder = "<img style='height:20px' src='/onyuz/standard/assets/img/sfClients/" +image_address + "'></img>";
-//                        return image_place_holder;
-//                    }
-//                }
             ]]
+
     });
+
     $('#reg_address_table').datagrid('enableFilter');
+
+
+
 });
 
