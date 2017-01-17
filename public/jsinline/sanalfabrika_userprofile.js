a2c19a0ebfde8d395490e27e71187b36320be705
Bahram <bahram.lotfisadigh@gmail.com>
2017-01-17 14:02:37 +0200

    son degisiklikler

--- public/jsinline/sanalfabrika_userprofile.js
+++ public/jsinline/sanalfabrika_userprofile.js
@@ -37,21 +37,40 @@ $(document).ready(function () {
 });
 
 
-$.ajax({
-    url:'https://proxy.sanalfabrika.com/SlimProxyBoot.php',
-    data:{
-        url:'',
-        pk:$('#pk').val(),
-        language_code: $('#langCode').val()
-    },
-    type:'GET',
-    datatype: 'json',
-    success:{
-        
-    },
-    error:{
-        
-    }
-            
-});
+/*
+         * User name on toolbar
+         */
+        $.ajax({
+            url: 'https://proxy.sanalfabrika.com/SlimProxyBoot.php',
+            data: {
+                url: 'pkFillUsersProfileInformation_infoUsers',
+                language_code: $("#langCode").val(),
+                pk: $('#pk').val()
+            },
+            type: 'GET',
+            dataType: 'json',
+            //data: 'rowIndex='+rowData.id,
+            success: function (data, textStatus, jqXHR) {
+                if (data.length !== 0) {
+                    
+                    $('#name').empty();
+                    $('#surname').empty();
+                    $('#pref_lang').empty();
+                    
+                    console.log(data.rows[0]);
+                    
+                    $('#name').append(data.rows[0].name);                    
+                    $('#surname').append(data.rows[0].surname);        
+                    $('#regdate').append(data.rows[0].registration_date);                
+                    $('#pref_lang').append(data.rows[0].preferred_language_name);
+                                    
+
+                } else {
+                    console.error('"consultants" servis datasÃ„Â± boÃ…Å¸tur!!');
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error('"pkGetUserShortInformation_infoUsers" servis hatasÃ„Â±->' + textStatus);
+            }
+        });
 
